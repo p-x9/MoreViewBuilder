85eb320fbdfd10b218aaf19269bc22a9b4f79d79
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-01-27 05:36:37 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# InfiniteViewBuilder
+
+A description of this package.
---
+++ Sources/InfiniteViewBuilder/InfiniteViewBuilder.swift
@@ -0,0 +1,6 @@
+public struct InfiniteViewBuilder {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
